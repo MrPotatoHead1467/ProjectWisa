ffb14d2f2f9b173669d0cf9f160e45395b2642a0
Maarten Van Beneden <maarten.vanbeneden@gmail.com>
2018-02-28 20:13:55 +0100

    Added multiple forms
    Added "ContactFormulier", "LoopbaanFormulier", "Persoonsformulier", "RelatiesFormulier"

---
+++ WISA-LoopbaanFormulier.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="KSLeuven" />
+    
+    <link href="Wisa-Layout.css" rel="stylesheet" type="text/css" />
+
+	<title>WISA | Loopbaan formulier</title>
+</head>
+
+<body>
+
+    <p>Loopbaan formulier</p>
+
+</body>
+</html>
---
+++ WISA-Persoonsformulier.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="KSLeuven" />
+    
+    <link href="Wisa-Layout.css" rel="stylesheet" type="text/css" />
+
+	<title>WISA | Persoonsformulier</title>
+</head>
+
+<body>
+
+    <p>Persoonsformulier</p>
+
+</body>
+</html>
