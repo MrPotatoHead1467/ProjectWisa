056d6885f9365ddefc3dd0d9742c11ef15d02d1e
Sarah Diana <diana@minoulip.name>
2018-03-23 15:11:53 +0100

    Aanpassingen layout Contact

--- WISA-ContactFormulier.php
+++ WISA-ContactFormulier.php
@@ -10,69 +10,64 @@
 </head>
 
 <body>
-<?php
-include "WISA-Connection.php";
-?>
-<form action="WISA-ContactFormulier_Check.php" method="post">
-    <!-- bestanden toevoegen -->
-    <!--
-    <div class="form_box_1">
-        <input class="form_bsd" id="Bestand_contact" name="Bestand_contact[]" multiple type="file"/>
-        <label class="form_bsdi" onclick="KlikKnop('Bestand_contact')" title="Document selecteren."></label>
-    </div>
-    -->
+    <?php
+    include "WISA-Connection.php";
+    ?>
     
-    <div>
-        <!-- Zoekvak persoon -->
-        <label for="Zoekvak">Zoeken</label><br />
-        <input type="text" id="Zoekvak" name="Zoekvak"/>
-        <button type='submit' id='Zoekvak_Zoeken' name='Zoekvak_Zoeken'>Zoeken</button><br />
-    </div>
+    <!-- bestanden toevoegen D1-->
     
+    
+    
+    <!-- Persoons gegevens zoeken -->
     <div>
-        <!-- Persoon -->
-        <label for="Persoon">Persoon</label><br />
-        <select id="Persoon" name="Persoon">
-            <option value="Kies">...</option>
-            <?php
-                /** Als er iets in het zoekvak is ingevuld, worden alleen de overige namen getoond. Als er niets is ingevuld,
-                    worden alle namen getoond */
-                if (isset($_SESSION['Zoekvak'])){
-                    $sql = $_SESSION['Zoekvak'];
-                }
-                else {
+        <form action="WISA-ContactFormulier_Check.php" method="post">
+            <div class="form_box_zoek">
+                <label class="form_lbl" for="Contact_Zoeken_in">Persoons gegevens zoeken</label><br />
+                <button class="form_edit" id="Contact_Zoeken_btn" name="Contact_Zoeken_btn" type="submit">Gegevens invullen</button>
+                <div class="form_zoek">
+                    <input class="form_in" id="Contact_Zoeken_in" list="Contact_Zoeken_List" name="Contact_Zoeken_in" placeholder="..." />
+                    <label class="form_editi" for="Contact_Zoeken_btn" onclick="KlikKnop('Contact_Zoeken_btn')" title="Personns gegevens zoekn."></label>
+                </div>
+                <datalist class="form_slt" id="Persoon_Zoeken_List" >
+                <?php
                     $sql = "SELECT * FROM tbl_personen";
-                }
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()){
-                        echo "<option value='".$row['fld_persoon_id']."'>".$row['fld_persoon_naam']." | ".$row['fld_persoon_gb_datum']."</option>";
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()){
+                            echo "<option id='".$row['fld_persoon_id']."' value='".$row['fld_persoon_naam']." (".$row['fld_persoon_gb_datum'].")'>";
+                        }
                     }
-                }
-            ?>
-        </select>
+                ?>
+            </datalist>
+            <input id="Persoon_Zoeken" name="Persoon_Zoeken" type="hidden"/>
+            <label class="form_bsdi" onclick="KlikKnop('Bestand_contact')" title="Document selecteren."></label>
+            </div>
+        </form>
     </div>
     
-    <div>
-        <label for="Straat">Straat</label><br />
-        <input type="text" id="Straat" name="Straat"/>
-    </div>
+    <form action="WISA-ContactFormulier_Check.php" method="post">
     
-    <div>
-        <label for="Huisnummer">Huisnummer</label><br />
-        <input type="text" id="Huisnummer" name="Huisnummer"/>
-    </div>
-    
-    <div>
-        <label for="Bus">Bus</label><br />
-        <input type="text" id="Bus" name="Bus"/>
-    </div>
-    
-    <div>
-        <div>
-            <label>Woonplaats</label><br />
-            <select id="Woonplaats_Lijst" name="Woonplaats_Lijst" onchange="woonplaats_niet_be()">
-                <option value="Kies">...</option>
+        <!-- bestanden toevoegen D2-->
+        <input class="form_bsd" id="Bestand_contact" name="Bestand_contact[]" multiple type="file"/>
+        
+        <!-- adres -->
+        <div class="form_box_1">
+            <label class="form_lbl" for="Straat">Adres</label><br />
+            <!-- straat -->
+            <div class="form_box_in">
+                <input id="Straat" name="Straat" placeholder="Straatnaam" type="text" />
+            </div>
+            <!-- huisnummer -->
+            <div class="form_box_in">
+                <input id="Huisnummer" name="Huisnummer" placeholder="Huisnummer" type="text"/>
+            </div>
+            <!-- bus -->
+            <div class="form_box_in">
+                <input id="Bus" name="Bus" placeholder="Bus" type="text"/>
+            </div>
+            <!-- woonplaats -->
+            <select class="form_slt" id="Woonplaats_Lijst" name="Woonplaats_Lijst" onchange="woonplaats_niet_be()">
+                <option value="Kies">Postcode en woonplaats</option>
                 <option value="Niet_BE">Woonplaats niet in Belgie</option>
                 <?php
                     $sql = "SELECT * FROM tbl_postcodes";
@@ -84,159 +79,150 @@ include "WISA-Connection.php";
                     }
                 ?>
             </select>
+            <!-- woonplaats niet België-->
+            <div id="Woonplaats_niet_be">
+                <div class="form_box_in">
+                    <input id="Woonplaats_niet_be_txt" name="Woonplaats_niet_be_txt" placeholder="Woonplaats (niet in België)" type="text"/>
+                </div>
+            </div>
+            <!-- land -->
+            <br />
+            <select class="form_slt" id="Land" name="Land">
+                <option value="Kies">Land</option>
+            </select>
+            <!-- beschrijving -->
+            <textarea class="form_in1" id="Besch_Woonplaats" name="Besch_Woonplaats" placeholder="Beschrijving woonplaats"></textarea> 
         </div>
         
-        <div class="Woonplaats_niet_be" id="Woonplaats_niet_be">
-            <label for="Woonplaats_niet_be_txt">Geef woonplaats in</label><br />
-            <input type="text" id="Woonplaats_niet_be_txt" name="Woonplaats_niet_be_txt"/>
+        <!-- GSM -->
+        <div class="form_box_1">
+            <label class="form_lbl" for="GSM">GSM</label><br />
+            <!-- nummer -->
+            <div class="form_box_in">
+                <input type="text" id="GSM" name="GSM" placeholder="GSM nummer"/>
+            </div>
+            <!-- toevoeg knop -->
+            <button class='form_pls1' id="GSM_Opslaan" name="GSM_Opslaan" type="submit">+</button>
+            <!-- beschrijving GSM nummer -->
+            <textarea class="form_in1" id="Besch_GSM" name="Besch_GSM" placeholder="Beschrijving GSM nummer"></textarea>
         </div>
-    </div>
-    
-    <div>
-        <label for="Land">Land</label>
-        <select id="Land" name="Land">
-            <option value="Kies">...</option>
-        </select>
-    </div>
-    
-    <div>
-        <label for="Besch_Woonplaats">Beschrijving woonplaats</label>
-        <textarea id="Besch_Woonplaats" name="Besch_Woonplaats"></textarea>
-    </div>
-    
-    <div>
-        <!-- Invoervak voor GSM nummer met opslaan knop -->
-        <div>
-            <div>
-                <label for="GSM">GSM</label><br />
-                <input type="text" id="GSM" name="GSM"/>
-            </div>
-            
-            <div>
-                <label for="Besch_GSM">Beschrijving GSM</label>
-                <textarea id="Besch_GSM" name="Besch_GSM"></textarea>
-            </div>
-            
-            <div>
-                <button type="submit" id="GSM_Opslaan" name="GSM_Opslaan">Opslaan</button>
-            </div>
-        </div>
-
-        <div id="Mogelijke_GSM" class="Mogelijke_GSM">
-            <?php
-                if (isset($_SESSION['Mogelijke_GSM_nrs']))
-                    {
-                        foreach ($_SESSION['Mogelijke_GSM_nrs'] as $Mogelijk_GSM_nr)
-                            {
-                                echo "<div>";
-                                    /** Verwijderknop */
-                                    echo "<button type='submit' id='".$Mogelijk_GSM_nr."' name='".$Mogelijk_GSM_nr."'>x</button>";
-                                    /** Mogelijk gsm nummer tonen in tekstvak */
-                                    echo "<label id='Mogelijke_GSM_nrs' name='Mogelijke_GSM_nrs[]'>".$Mogelijk_GSM_nr."</label><br />";
-                                echo '</div>';
-                            }   
-                    }
-            ?>
-        </div>
-    </div>
-    
-    <div>
-        <div>
-            <div>
-                <label for="Telefoon">Telefoon</label><br />
-                <input type="text" id="Telefoon" name="Telefoon"/>
-            </div>
-            
-            <div>
-                <label for="Besch_Tel">Beschrijving telefoon</label>
-                <textarea id="Besch_Tel" name="Besch_Tel"></textarea>
-            </div>
-            
-            <div>
-                <button type="submit" id="Telefoon_Opslaan" name="Telefoon_Opslaan">Opslaan</button>
+        
+        <!-- Bestaande GSM nummers -->
+        <div class="form_box_1">
+            <div id="Mogelijke_GSM" class="Mogelijke_GSM">
+                <?php
+                    if (isset($_SESSION['Mogelijke_GSM_nrs']))
+                        {
+                            foreach ($_SESSION['Mogelijke_GSM_nrs'] as $Mogelijk_GSM_nr)
+                                {
+                                    echo "<div>";
+                                        /** Verwijderknop */
+                                        echo "<div class='form_box_in'>";
+                                            echo "<button class='form_mn' id='".$Mogelijk_GSM_nr."' name='".$Mogelijk_GSM_nr."' type='submit'>x</button>";
+                                            /** Mogelijk gsm nummer tonen in tekstvak */
+                                            echo "<label class='form_lbl' id='Mogelijke_GSM_nrs' name='Mogelijke_GSM_nrs[]'>".$Mogelijk_GSM_nr."</label><br />";
+                                        echo "</div>";
+                                    echo '</div>';
+                                }   
+                        }
+                ?>
             </div>
         </div>
         
-        <div id="Mogelijke_Tel" class="Mogelijke_Tel">
+        <!-- telefoon -->
+        <div class="form_box_1">
+            <label class="form_lbl" for="Telefoon">Telefoon</label><br />
+            <!-- telefoon nummer -->
+            <div class="form_box_in">
+                <input id="Telefoon" name="Telefoon" placeholder="Telefoon nummer" type="text"/>
+            </div>
+            <!-- toevoeg knop -->
+            <button class='form_pls1' id="Telefoon_Opslaan" name="Telefoon_Opslaan" type="submit">+</button>
+            <!-- beschrijving -->
+            <textarea class="form_in1" id="Besch_Tel" name="Besch_Tel" placeholder="Beschrijving telefoon nummer"></textarea>
+        </div>
+                
+        <!-- toegevoegde telefoon nummers -->  
+        <div class="form_box_1" id="Mogelijke_Tel">
             <?php
                 if (isset($_SESSION['Mogelijke_Tel_nrs']))
                     {
                         foreach ($_SESSION['Mogelijke_Tel_nrs'] as $Mogelijk_Tel_nr)
                             {
                                 echo "<div>";
-                                    /** Verwijderknop */
-                                    echo "<button type='submit' id='".$Mogelijk_Tel_nr."' name='".$Mogelijk_Tel_nr."'>x</button>";
-                                    /** Mogelijk telefoon nummer tonen in tekstvak */
-                                    echo "<label id='Mogelijke_Tel_nrs' name='Mogelijke_Tel_nrs[]'>".$Mogelijk_Tel_nr."</label><br />";
+                                    echo "<div class='form_box_in'>";
+                                        /** Verwijderknop */
+                                        echo "<button class='form_mn' id='".$Mogelijk_Tel_nr."' name='".$Mogelijk_Tel_nr."' type='submit'>x</button>";
+                                        /** Mogelijk telefoon nummer tonen in tekstvak */
+                                        echo "<label class='form_lbl' id='Mogelijke_Tel_nrs' name='Mogelijke_Tel_nrs[]'>".$Mogelijk_Tel_nr."</label><br />";
+                                    echo "</div>";
                                 echo '</div>';
                             }   
                     }
             ?>
         </div>
-    </div>
-    
-    <div>
-        <div>
-            <div>
-                <label for="Email">Email</label><br />
-                <input type="text" id="Email" name="Email"/>
-            </div>
-            
-            <div>
-                <label for="Besch_GSM">Beschrijving GSM</label>
-                <textarea id="Besch_GSM" name="Besch_GSM"></textarea>
-            </div>
-            
-            <div>
-                <button type="submit" id="Email_Opslaan" name="Email_Opslaan">Opslaan</button>
-            </div>
-        </div>
         
-        <div id="Mogelijke_Email" class="Mogelijke_Email">
+        <!-- e-mail -->
+        <div class="form_box_1">
+            <label class="form_lbl" for="Email">Email</label><br />
+            <!-- E-mail adres -->
+            <div class="form_box_in">
+                <input id="Email" name="Email" placeholder="E-mail adres" type="text"/>
+            </div>
+            <!-- toevoeg knop -->
+            <button class='form_pls1' id="Email_Opslaan" name="Email_Opslaan" type="submit">+</button>
+            <!-- Beschrijving E-mail adres -->
+            <textarea class="form_in1" id="Besch_GSM" name="Besch_GSM"></textarea>
+        </div>
+                      
+        <!-- bestaande e-mail adressen -->    
+        <div class="form_box_1" id="Mogelijke_Email">
             <?php
                 if (isset($_SESSION['Mogelijke_Emailadressen']))
                     {
                         foreach ($_SESSION['Mogelijke_Emailadressen'] as $Mogelijk_Emailadres)
                             {
                                 echo "<div>";
-                                    /** Verwijderknop */
-                                    echo "<button type='submit' id='".$Mogelijk_Emailadres."' name='".$Mogelijk_Emailadres."'>x</button>";
-                                    /** Mogelijk emailadres tonen in tekstvak */
-                                    echo "<label id='Mogelijke_Emailadressen' name='Mogelijke_Emailadressen[]'>".$Mogelijk_Emailadres."</label><br />";
+                                    echo "<div class='form_box_in'>";
+                                        /** Verwijderknop */
+                                        echo "<button class='form_mn' id='".$Mogelijk_Emailadres."' name='".$Mogelijk_Emailadres."' type='submit'>x</button>";
+                                        /** Mogelijk emailadres tonen in tekstvak */
+                                        echo "<label class='form_lbl' id='Mogelijke_Emailadressen' name='Mogelijke_Emailadressen[]'>".$Mogelijk_Emailadres."</label><br />";
+                                    echo "</div>";
                                 echo '</div>';
                             }   
                     }
             ?>
         </div>
-    </div>
+        
+        
+        <div class="form_box_1">
+            <!-- Contact opslaan knop -->
+            <button class="form_btn" id="Contact_Opslaan" name="Contact_Opslaan" type="submit">Opslaan</button>
+            <!-- Volgende knop -->
+            <button class="form_ccl" id="Volgende" name="Volgende" title="Volgende formulier: Loopbaan."type="submit">Volgende</button>
+        </div>
+    </form>
+
+    <script type="text/javascript">
+    <!--
+            function KlikKnop(knop)
+            {
+                document.getElementById(knop).click();
+            }
     
-    <div>
-        <!-- Contact opslaan knop -->
-        <button type="submit" id="Contact_Opslaan" name="Contact_Opslaan">Opslaan</button>
-        <!-- Volgende knop -->
-        <button type="submit" id="Volgende" name="Volgende">Volgende</button>
-    </div>
-</form>
-
-<script type="text/javascript">
-<!--
-        function KlikKnop(knop)
-        {
-            document.getElementById(knop).click();
+    	function woonplaats_niet_be() {
+                var woonplaats = document.getElementById("Woonplaats_Lijst");
+                var selectedValue = woonplaats.options[woonplaats.selectedIndex].value;
+                if (woonplaats.value == "Niet_BE") {
+                    document.getElementById('Woonplaats_niet_be').style.display = 'block';
+                }
+                else {
+                    document.getElementById('Woonplaats_niet_be').style.display = 'none';
+                }
         }
-
-	function woonplaats_niet_be() {
-            var woonplaats = document.getElementById("Woonplaats_Lijst");
-            var selectedValue = woonplaats.options[woonplaats.selectedIndex].value;
-            if (woonplaats.value == "Niet_BE") {
-                document.getElementById('Woonplaats_niet_be').style.display = 'block';
-            }
-            else {
-                document.getElementById('Woonplaats_niet_be').style.display = 'none';
-            }
-    }
--->
-</script>
+    -->
+    </script>
 
 </body>
 </html>
