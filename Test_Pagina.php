d5b0334d4c9b5f6ebe10e5b915ecf7ff9d08bf8a
Maarten Van Beneden <Maarten.vanbeneden@gmail.com>
2018-04-25 12:08:29 +0200

    Can add files to 'persoonsformulier'

--- Test_Pagina.php
+++ Test_Pagina.php
@@ -1,48 +1,23 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html" />
-	<meta name="author" content="KSLeuven" />
-    
-    <link href="Wisa-Layout.css" rel="stylesheet" type="text/css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-	<title>WISA | Test formulier</title>
-</head>
-
-<body>
 <?php
-include "WISA-Connection.php";
+$Naam = "Maarten Van Beneden";
+$Persoon_Id = 13;
+$target_dir = 'Uploads/'.$Naam.'_'.$Persoon_Id;
+if (!file_exists($target_dir)) {
+    mkdir($target_dir, 0777, true);
+}
+if (!file_exists($target_dir.'/Persoon')) {
+    mkdir($target_dir.'/Persoon', 0777, true);
+}
+if (!file_exists($target_dir.'/Relatie')) {
+    mkdir($target_dir.'/Relatie', 0777, true);
+}
+if (!file_exists($target_dir.'/Contact')) {
+    mkdir($target_dir.'/Contact', 0777, true);
+}
+if (!file_exists($target_dir.'/Loopbaan')) {
+    mkdir($target_dir.'/Loopbaan', 0777, true);
+}
+if (!file_exists($target_dir.'/Inschrijving')) {
+    mkdir($target_dir.'/Inschrijving', 0777, true);
+}
 ?>
-
-<!-- school -->
-<div class="form_box_1">
-    <label class="form_lbl" for="School">School</label><br />
-    
-    <div class="form_zoek">
-        <input class="form_in" id="School_Zoeken_in" list="School_Zoeken_List" name="School_Zoeken_in" placeholder="..." />
-    </div>
-    <datalist class="form_slt" id="School_Zoeken_List" >
-        <?php
-            $sql = "SELECT tbl_scholen.fld_school_naam, tbl_scholen.fld_school_id, tbl_adressen_linken.fld_adres_id_fk, tbl_adressen.fld_adres_id, tbl_postcodes.fld_postnummer, tbl_postcodes.fld_woonplaats_naam
-                    FROM (((tbl_scholen
-                    INNER JOIN tbl_adressen_linken ON tbl_scholen.fld_school_id = tbl_adressen_linken.fld_school_id_fk)
-                    INNER JOIN tbl_adressen ON tbl_adressen_linken.fld_adres_id_fk = tbl_adressen.fld_adres_id)
-                    INNER JOIN tbl_postcodes ON tbl_adressen.fld_adres_postcode_id_fk = tbl_postcodes.fld_postcode_id);";
-                    
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0)
-                {
-                    while($row = mysqli_fetch_assoc($result))
-                        {              
-                            echo "<option id='".$row['fld_school_id']."' value='".$row['fld_school_naam']." | ".$row['fld_postnummer']."(".$row['fld_woonplaats_naam'].")'>";
-                        }
-                }
-        ?>
-    </datalist>
-    <input id="School_Zoeken" name="School_Zoeken" type="hidden"/>
-</div>
-
-
-</body>
-</html>
--- WISA-Inschrijvingsformulier_Check.php
+++ WISA-Inschrijvingsformulier_Check.php
@@ -5,38 +5,66 @@ $_SESSION['Leerling'] = 13;
 if(isset($_POST["Inschrijving_Opslaan"])) {
     $Persoon_Id = $_SESSION['Leerling'];
     $Datum = date("Y-m-d_H-i");
-    $target_dir = "Uploads/";
+    $sqlPersoon = "SELECT * FROM tbl_personen WHERE fld_persoon_id=".$Persoon_Id;
+    $resultPersoon = mysqli_query($conn, $sqlPersoon);
+    if (mysqli_num_rows($resultPersoon) > 0) {
+        while($rowPersoon = mysqli_fetch_assoc($resultPersoon)){
+            $Persoon_Naam = $rowPersoon['fld_persoon_naam'];
+        }
+    }
+    
     $sqlVragen = "SELECT * FROM tbl_vragen";
     $resultVragen = mysqli_query($conn, $sqlVragen);
     if (mysqli_num_rows($resultVragen) > 0) {
         while($rowVragen = mysqli_fetch_assoc($resultVragen)){
-            if (isset($_FILES[$rowVragen['fld_vraag_id']."_Bestand"]) and $_FILES[$rowVragen['fld_vraag_id']."_Bestand"] != ''){
-                $Bestand = $_FILES[$rowVragen['fld_vraag_id']."_Bestand"];
+            $Vraag_ID = $rowVragen['fld_vraag_id'];
+            $Dir = "Uploads/".$Persoon_Naam."_".$Persoon_Id."/Inschrijving/".$Vraag_ID;
+            $target_dir = $Dir."/";
+            if (isset($_FILES[$Vraag_ID."_Bestand"])){
+                
+                $Bestand = $_FILES[$Vraag_ID."_Bestand"];
                 $Aantal_Bestanden = count($Bestand['name']);
                 for ($i = 0; $i < $Aantal_Bestanden; $i++){
-                    $Bestand_Basename = pathinfo($Bestand["name"][$i], PATHINFO_FILENAME);
-                    $Soort_Bestand = pathinfo($Bestand["name"][$i], PATHINFO_EXTENSION);
-                    $Bestand_Naam = $Bestand_Basename."_".$rowVragen['fld_vraag_id']."_".$Persoon_Id.".".$Soort_Bestand;
-                    $Bestand_Locatie = $target_dir.$Bestand_Naam;
-                    /** Het bestand wordt ge�pload */
-                    if (move_uploaded_file($_FILES[$rowVragen['fld_vraag_id']."_Bestand"]["tmp_name"][$i], $Bestand_Locatie)) {
-                        echo "Het bestand ". basename($Bestand["name"][$i]). " is ge�pload.<br />";
-                        
-                        $sqlBestanden = "INSERT INTO tbl_docs(fld_doc_naam, fld_doc_soort, fld_doc_plaats, fld_doc_datum) VALUES ('".$Bestand_Naam."', '".$Soort_Bestand."', '".$Bestand_Locatie."', '".$Datum."')";
-                        if (mysqli_query($conn, $sqlBestanden)){
-                            echo "Gelukt";
+                    if ($Bestand['size'][$i] != 0){
+                        if (!file_exists($Dir)) {
+                            mkdir($Dir, 0777, true);
+                        }
+                
+                        $Bestand_Basename = pathinfo($Bestand["name"][$i], PATHINFO_FILENAME);
+                        $Soort_Bestand = pathinfo($Bestand["name"][$i], PATHINFO_EXTENSION);
+                        $Bestand_Naam = $Bestand_Basename."_".$Vraag_ID."_".$Persoon_Id.".".$Soort_Bestand;
+                        $Bestand_Locatie = $target_dir.$Bestand_Naam;
+                        /** Het bestand wordt geüpload */
+                        if (move_uploaded_file($_FILES[$Vraag_ID."_Bestand"]["tmp_name"][$i], $Bestand_Locatie)) {
+                            echo "Het bestand ".$Bestand_Basename.".".$Soort_Bestand." is ge�pload.<br />";
+                            
+                            $sqlBestanden = "INSERT INTO tbl_docs(fld_doc_naam, fld_doc_soort, fld_doc_plaats, fld_doc_datum) 
+                                             VALUES ('".$Bestand_Naam."', '".$Soort_Bestand."', '".$Bestand_Locatie."', '".$Datum."')";
+                            
+                            if (mysqli_query($conn, $sqlBestanden)){
+                                $Doc_Id = mysqli_insert_id($conn);
+                                $sqlDoc_link = "INSERT INTO tbl_docs_links (fld_doc_id_fk, fld_persoon_id_fk, fld_vraag_id_fk)
+                                                VALUES ('".$Doc_Id."', '".$Persoon_Id."', '".$Vraag_ID."')";
+                                                
+                                if (mysqli_query($conn, $sqlDoc_link)){
+                                    
+                                }
+                                else {
+                                    echo "Error: " . $sqlDoc_link . "<br>" . mysqli_error($conn);
+                                }
+                            }
+                            else {
+                                echo "Error: " . $sqlBestanden . "<br>" . mysqli_error($conn);
+                            }
                         }
                         else {
-                            echo "Error: " . $sqlBestanden . "<br>" . mysqli_error($conn);
+                            echo "Bestand is niet geüpload.<br />";
                         }
-                    }
-                    else {
-                        
                     }
                 }
             }
             
-            $Vraag_ID = $rowVragen['fld_vraag_id'];
+            
             
             if ($rowVragen['fld_antwoord_type_doc'] != 1 && $rowVragen['fld_antwoord_type_foto'] != 1){
                 $Antwoord = mysqli_real_escape_string($conn, $_POST[$Vraag_ID]);
@@ -69,12 +97,16 @@ if(isset($_POST["Inschrijving_Opslaan"])) {
             // foto vraag
             elseif ($rowVragen['fld_antwoord_type_foto'] == 1)
                 {
-                    if (isset($_FILES["Foto_".$rowVragen['fld_vraag_id']]) and $_FILES["Foto_".$rowVragen['fld_vraag_id']] != ''){
-                        $Foto = $_FILES["Foto_".$rowVragen['fld_vraag_id']];
+                    if (isset($_FILES["Foto_".$Vraag_ID]) and $_FILES["Foto_".$Vraag_ID]['size'] != 0){
+                        if (!file_exists($Dir)) {
+                            mkdir($Dir, 0777, true);
+                        }
+                        
+                        $Foto = $_FILES["Foto_".$Vraag_ID];
                         
                         $Soort_Foto = pathinfo($Foto["name"], PATHINFO_EXTENSION);
                         $Foto_Basename = pathinfo($Foto["name"], PATHINFO_FILENAME);
-                        $Foto_Naam = $Foto_Basename."_".$rowVragen['fld_vraag_id']."_".$Persoon_Id;
+                        $Foto_Naam = $Foto_Basename."_".$Vraag_ID."_".$Persoon_Id;
                         
                         $Foto_Locatie = $target_dir . $Foto_Naam.".".$Soort_Foto;
                         echo "Foto: ".$Foto_Naam."<br />";
@@ -112,15 +144,19 @@ if(isset($_POST["Inschrijving_Opslaan"])) {
             // doc vraag 
             elseif ($rowVragen['fld_antwoord_type_doc'] == 1)
                 {
-                    if (isset($_FILES["Document_".$rowVragen['fld_vraag_id']]) and $_FILES["Document_".$rowVragen['fld_vraag_id']] != ''){
-                        $Bestand = $_FILES["Document_".$rowVragen['fld_vraag_id']];
+                    if (isset($_FILES["Document_".$Vraag_ID]) and $_FILES["Document_".$Vraag_ID]['size'] != 0){
+                        if (!file_exists($Dir)) {
+                            mkdir($Dir, 0777, true);
+                        }
+                        
+                        $Bestand = $_FILES["Document_".$Vraag_ID];
                         
                         $Soort_Bestand = pathinfo($Bestand["name"], PATHINFO_EXTENSION);
                         $Bestand_Basename = pathinfo($Bestand["name"], PATHINFO_FILENAME);
-                        $Bestand_Naam = $Bestand_Basename."_".$rowVragen['fld_vraag_id']."_".$Persoon_Id;
+                        $Bestand_Naam = $Bestand_Basename."_".$Vraag_ID."_".$Persoon_Id;
                         
                         $Bestand_Locatie = $target_dir . $Bestand_Naam.".".$Soort_Bestand;
-                        echo $Bestand_Naam;
+                        echo "Bestand: ".$Bestand_Naam;
                         
                         if (move_uploaded_file($Bestand["tmp_name"], $Bestand_Locatie)) {
                             echo "Het bestand ". basename($Bestand["name"]). " is geüpload.<br />";
