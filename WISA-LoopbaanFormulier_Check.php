13841c1fe13ea7d67f276e3467eb942f87d7fa37
Maarten Van Beneden <Maarten.vanbeneden@gmail.com>
2018-04-27 15:27:00 +0200

    Can now add 'attest'

--- WISA-LoopbaanFormulier_Check.php
+++ WISA-LoopbaanFormulier_Check.php
@@ -33,21 +33,71 @@ if (isset($_POST['Loopbaan_Opslaan'])){
                         VALUES ('".$Persoon_Id."', '".$School_Id."', '".$Richting."', '".$Schooljaar."', '".$Begindatum."', '".$Einddatum."')";
     }
     
+    
+            
     if (mysqli_query($conn, $sqlLoopbaan)){
-        if (isset($_FILES["Bestand_loopbaan"])){
-            $Datum = date("Y-m-d_H-i");
-            $sqlPersoon = "SELECT * FROM tbl_personen WHERE fld_persoon_id=".$Persoon_Id;
-            $resultPersoon = mysqli_query($conn, $sqlPersoon);
-            if (mysqli_num_rows($resultPersoon) > 0) {
-                while($rowPersoon = mysqli_fetch_assoc($resultPersoon)){
-                    $Persoon_Naam = $rowPersoon['fld_persoon_naam'];
-                }
+        $Loopbaan_Id = mysqli_insert_id($conn);
+        $Datum = date("Y-m-d_H-i");
+        $sqlPersoon = "SELECT * FROM tbl_personen WHERE fld_persoon_id=".$Persoon_Id;
+        $resultPersoon = mysqli_query($conn, $sqlPersoon);
+        if (mysqli_num_rows($resultPersoon) > 0) {
+            while($rowPersoon = mysqli_fetch_assoc($resultPersoon)){
+                $Persoon_Naam = $rowPersoon['fld_persoon_naam'];
             }
-            $Dir = "Uploads/".$Persoon_Naam."_".$Persoon_Id."/Loopbaan";
-            $target_dir = $Dir."/";
+        }
+        
+        $Dir = "Uploads/".$Persoon_Naam."_".$Persoon_Id."/Loopbaan";
+        $target_dir = $Dir."/";
+        
+        if (isset($_FILES["Attest_Doc"])){
             if (!file_exists($Dir)) {
                 mkdir($Dir, 0777, true);
             }
+            
+            $Bestand = $_FILES["Attest_Doc"];
+            $Aantal_Bestanden = count($Bestand['name']);
+            $Bestand_Basename = pathinfo($Bestand["name"], PATHINFO_FILENAME);
+            $Soort_Bestand = pathinfo($Bestand["name"], PATHINFO_EXTENSION);
+            $Bestand_Naam = $Bestand_Basename."_Attest_".$Persoon_Id.".".$Soort_Bestand;
+            $Bestand_Locatie = $target_dir.$Bestand_Naam;
+            echo "Bestand_Locatie: ".$Bestand_Locatie."<br />";
+            /** Het bestand wordt geüpload */
+            if ($Bestand['size'] != 0){
+                if (move_uploaded_file($Bestand["tmp_name"], $Bestand_Locatie)) {
+                    echo "Het bestand ". basename($Bestand["name"]). " is geüpload.<br />";
+                    
+                    $sqlBestanden = "INSERT INTO tbl_docs(fld_doc_naam, fld_doc_soort, fld_doc_plaats, fld_doc_datum) 
+                                     VALUES ('".$Bestand_Naam."', '".$Soort_Bestand."', '".$Bestand_Locatie."', '".$Datum."')";
+                    if (mysqli_query($conn, $sqlBestanden)){
+                        $Doc_Id = mysqli_insert_id($conn);
+                        $sqlDoc_link = "INSERT INTO tbl_docs_links (fld_doc_id_fk, fld_persoon_id_fk, fld_loopbaan_id_fk)
+                                        VALUES ('".$Doc_Id."', '".$Persoon_Id."', '".$Loopbaan_Id."')";
+                                        
+                        if (mysqli_query($conn, $sqlDoc_link)){
+                            
+                        }
+                        else {
+                            echo "Error: " . $sqlDoc_link . "<br>" . mysqli_error($conn);
+                        }
+                    }
+                    else {
+                        echo "Error: " . $sqlBestanden . "<br>" . mysqli_error($conn);
+                    }
+                }
+                else {
+                    echo "Bestand niet geüpload";
+                }
+            }
+        }
+        else {
+            echo "Geen bestanden<br />";
+        }
+        
+        if (isset($_FILES["Bestand_loopbaan"])){
+            if (!file_exists($Dir)) {
+                mkdir($Dir, 0777, true);
+            }
+            
             $Bestand = $_FILES["Bestand_loopbaan"];
             $Aantal_Bestanden = count($Bestand['name']);
             for ($i = 0; $i < $Aantal_Bestanden; $i++){
@@ -65,8 +115,8 @@ if (isset($_POST['Loopbaan_Opslaan'])){
                                          VALUES ('".$Bestand_Naam."', '".$Soort_Bestand."', '".$Bestand_Locatie."', '".$Datum."')";
                         if (mysqli_query($conn, $sqlBestanden)){
                             $Doc_Id = mysqli_insert_id($conn);
-                            $sqlDoc_link = "INSERT INTO tbl_docs_links (fld_doc_id_fk, fld_persoon_id_fk)
-                                            VALUES ('".$Doc_Id."', '".$Persoon_Id."')";
+                            $sqlDoc_link = "INSERT INTO tbl_docs_links (fld_doc_id_fk, fld_persoon_id_fk, fld_loopbaan_id_fk)
+                                        VALUES ('".$Doc_Id."', '".$Persoon_Id."', '".$Loopbaan_Id."')";
                                             
                             if (mysqli_query($conn, $sqlDoc_link)){
                                 
